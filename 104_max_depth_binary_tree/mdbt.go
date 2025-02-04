640b92ab1330b828a4b0647eccd5cf268176f3c4
Peyman Hassanabadi <hassanabadi.peyman@gmail.com>
2025-02-04 16:37:17 +0330

    Problem 104: Maximum Depth of Binary Tree

---
+++ 104_max_depth_binary_tree/mdbt.go
@@ -0,0 +1,21 @@
+package maxdepthbinarytree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func max(x int, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
