e94a840c692d6b604fc6648cd7cf67c6f1086125
Peyman Hassanabadi <hassanabadi.peyman@gmail.com>
2025-02-04 12:11:57 +0330

    Problem 141: Linked List Cycle added

--- 141_linked_list_cycle/llc.go
+++ 141_linked_list_cycle/llc.go
@@ -24,3 +24,27 @@ func HasCycle(head *ListNode) bool {
 	}
 	return false
 }
+
+/* If you wanna test
+l1 := linkedlistcycle.ListNode{
+	Val: 1,
+}
+
+l2 := linkedlistcycle.ListNode{
+	Val: 2,
+}
+
+l3 := linkedlistcycle.ListNode{
+	Val: 3,
+}
+
+l4 := linkedlistcycle.ListNode{
+	Val: 4,
+}
+
+l1.Next = &l2
+l2.Next = &l3
+l3.Next = &l4
+l4.Next = &l2
+
+*/
