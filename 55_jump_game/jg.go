3c3e41e088018a32b1109b1017140449ca98f95a
peyman.hassanabadi <peyman.hassanabadi@gandom.co>
2025-04-29 17:35:21 +0330

    Problem 55: jump game added

---
+++ 55_jump_game/jg.go
@@ -0,0 +1,18 @@
+package jumpgame
+
+func canJump(nums []int) bool {
+	gas := 0
+
+	for _, n := range nums {
+		if gas < 0 {
+			return false
+		}
+
+		if gas < n {
+			gas = n
+		}
+
+		gas -= 1
+	}
+	return true
+}
