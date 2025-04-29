8967e39824c242f9cde542c4ebaeb6d05894de38
peyman.hassanabadi <peyman.hassanabadi@gandom.co>
2025-04-29 17:00:30 +0330

    Problem 108: convert soreted array to binary tree added

---
+++ 108_convert_array_to_binary_tree/catbt.go
@@ -0,0 +1,18 @@
+package convertarraytobinarytree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	med := len(nums) / 2
+
+	root := TreeNode{
+		Val: nums[med],
+	}
+	root.Left = sortedArrayToBST(nums[:med])
+	root.Right = sortedArrayToBST(nums[med+1:])
+	return &root
+}
